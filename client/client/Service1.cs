184a976fd828e81c4e61f0468b3dfda1c8d7b943
gk <guoguo882010@hotmail.com>
2018-01-06 10:19:38 +0800

    create

---
+++ client/client/Service1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+using System.Web;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace client
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread _thread = new Thread(go);
+            _thread.Name = "My Worker Thread";
+            _thread.IsBackground = true;
+            _thread.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        private static void go()
+        {
+            while (true)
+            {
+                StartClient();
+                Thread.Sleep(5000);
+            }
+        }
+
+        public static void StartClient()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new byte[1024];
+
+            // Connect to a remote device.  
+            try
+            {
+                // Establish the remote endpoint for the socket.  
+                // This example uses port 11000 on the local computer.  
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                //IPAddress ipAddress = ipHostInfo.AddressList[0];
+                IPAddress ipAddress = IPAddress.Parse("111.111.111.111"); // ip adress
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11111);
+
+                // Create a TCP/IP  socket.  
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connect the socket to the remote endpoint. Catch any errors.  
+                try
+                {
+                    string _ip = GetIPAddress();
+
+                    if (_ip != "" && CheckIPValid(_ip))
+                    {
+                        sender.Connect(remoteEP);
+
+                        // Encode the data string into a byte array.  
+                        byte[] msg = Encoding.ASCII.GetBytes(_ip+"<EOF>");
+                        // Send the data through the socket.  
+                        int bytesSent = sender.Send(msg);
+                        // Release the socket.  
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+                    }
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    //Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                   // Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static string GetIPAddress()
+        {
+            // 获取本机ip地址
+            String address = "";
+            WebRequest request = WebRequest.Create("http://ip.cn"); // get ip adress example http://ip.cn
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    address = stream.ReadToEnd();
+                }
+
+                address = address.Trim();
+
+                return address;
+            }
+            catch
+            {
+                return "";
+            }
+
+        }
+
+        private static Boolean CheckIPValid(String strIP)
+        {
+            // Split string by ".", check that array length is 3
+            char chrFullStop = '.';
+            string[] arrOctets = strIP.Split(chrFullStop);
+            if (arrOctets.Length != 4)
+            {
+                return false;
+            }
+            // Check each substring checking that the int value is less than 255 
+            // and that is char[] length is !>     2
+            Int16 MAXVALUE = 255;
+            Int32 temp; // Parse returns Int32
+            foreach (String strOctet in arrOctets)
+            {
+                if (strOctet.Length > 3)
+                {
+                    return false;
+                }
+
+                temp = int.Parse(strOctet);
+                if (temp > MAXVALUE)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ server/server/Service1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.Web.Administration;
+using System.Threading;
+using System.IO;
+
+namespace server
+{
+    public partial class Service1 : ServiceBase
+    {
+        public static string data = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Thread _thread = new Thread(StartListening);
+            _thread.Name = "My Worker Thread";
+            _thread.IsBackground = true;
+            _thread.Start();
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        public static void StartListening()
+        {
+            // Data buffer for incoming data.  
+            byte[] bytes = new Byte[1024];
+
+            // Establish the local endpoint for the socket.  
+            // Dns.GetHostName returns the name of the   
+            // host running the application.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            //IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPAddress ipAddress = IPAddress.Parse("111.111.111.111");
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11111);
+
+            // Create a TCP/IP socket.  
+            Socket listener = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and   
+            // listen for incoming connections.  
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.  
+                while (true)
+                {
+                    // Console.WriteLine("Waiting for a connection...");
+                    // Program is suspended while waiting for an incoming connection.  
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    // An incoming connection needs to be processed.  
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        if (data.IndexOf("<EOF>") > -1)
+                        {
+                            string ip = data.Replace("<EOF>", "");
+
+                            if (!findBlockIp(ip.Trim(), "default site"))
+                            {
+                                addBlockIp(ip.Trim(), "default site");
+                            }
+
+                            
+
+                            break;
+                        }
+                    }
+
+                    // Show the data on the console.  
+                    // Console.WriteLine("Text received : {0}", data);
+
+                    // Echo the data back to the client.  
+                    // byte[] msg = Encoding.ASCII.GetBytes(data);
+
+                    // handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                // Console.WriteLine(e.ToString());
+            }
+
+        }
+
+        private static bool findBlockIp(String ip,String site)
+        {
+            using (ServerManager serverManager = new ServerManager())
+            {
+                Configuration config = serverManager.GetApplicationHostConfiguration();
+                ConfigurationSection ipSecuritySection = config.GetSection("system.webServer/security/ipSecurity", site);
+                ConfigurationElementCollection ipSecurityCollection = ipSecuritySection.GetCollection();
+
+                foreach (var item1 in ipSecurityCollection)
+                {
+                    if ((String)item1["ipAddress"] == ip)
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+
+            }
+        }
+
+        private static void addBlockIp(String ip,String site)
+        {
+            using (ServerManager serverManager = new ServerManager())
+            {
+                Configuration config = serverManager.GetApplicationHostConfiguration();
+                ConfigurationSection ipSecuritySection = config.GetSection("system.webServer/security/ipSecurity", site);
+                ConfigurationElementCollection ipSecurityCollection = ipSecuritySection.GetCollection();
+
+                ConfigurationElement addElement = ipSecurityCollection.CreateElement("add");
+                addElement["ipAddress"] = ip;
+                addElement["allowed"] = true;
+                ipSecurityCollection.Add(addElement);
+
+                serverManager.CommitChanges();
+            }
+        }
+
+        private static void WriteToFile(string text)
+        {
+            string path = "D:\\ServiceLog.txt";
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(string.Format(text, DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                writer.Close();
+            }
+        }
+    }
+}
